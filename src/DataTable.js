412e6393f6e5a896cad512bf8b31be1cc264c0f5
IGilfillan <igwebconsulting@gmail.com>
2017-08-10 21:50:54 +0100

    add all results data and styles

--- src/DataTable.js
+++ src/DataTable.js
@@ -4,12 +4,29 @@ let DataTable = (props) => {
 
   let rows = [];
 
+  const TYPES = {
+    listedEntity: 'Listed Entity',
+    isPEVCHouse: 'PEVC House'
+  };
+
   props.data.map((item, index) => {
 
     rows.push(
       <tr className="results__row" key={index}>
-        <td className="results__td">
-          {item.name}
+        <td className="results__td results__company">
+          <p className="results__name">{item.name}</p>
+          <p className="results__description">{item.description}</p>
+        </td>
+        <td className="results__td results__type">
+          <p className="results__listed">{item.isListedEntity ? TYPES.listedEntity : 'NOT Listed'}</p>
+          <p className="results__pevc">{item.isPEVCHouse ? TYPES.isPEVCHouse : 'Not PEVC'}</p>
+        </td>
+        <td className="results__td results__hq">
+          <span className="results__state">
+            {item.geography.state ? `${item.geography.state}, `: '' }
+          </span>
+          <span className="results__country">{item.geography.country}
+          </span>
         </td>
       </tr>
     );
@@ -21,6 +38,13 @@ let DataTable = (props) => {
 
   return (
     <table className="results">
+      <thead>
+      <tr>
+        <th className="results__th">Name</th>
+        <th className="results__th">Type</th>
+        <th className="results__th">Headquarter</th>
+      </tr>
+      </thead>
       <tbody>{rows}</tbody>
     </table>);
 
