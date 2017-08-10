1c7f4a88aaafa81e6aa3cf3de7f926c2f5186e0d
IGilfillan <igwebconsulting@gmail.com>
2017-08-10 21:45:22 +0100

    show only matching items

--- src/App.js
+++ src/App.js
@@ -13,11 +13,14 @@ class App extends Component {
 
     this.state = {
       items: [],
-      visibleItemCount: 5
+      visibleItemCount: 0
     };
 
     this.processResponse = this.processResponse.bind(this);
     this.showMore = this.showMore.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.resetState = this.resetState.bind(this);
+    this.updateSearchString = this.updateSearchString.bind(this);
 
   }
 
@@ -50,7 +53,22 @@ class App extends Component {
 
   getItems() {
 
-    return this.state.items.slice(0, this.state.visibleItemCount);
+    // look up matching items
+    let matchingItems = this.state.matchingIndices.map( (item) => {
+
+      return this.state.items[item];
+
+    });
+
+    return matchingItems.slice(0, this.state.visibleItemCount);
+
+  }
+
+  updateSearchString(string){
+
+    this.setState({
+      searchValue: string
+    });
 
   }
 
@@ -68,6 +86,57 @@ class App extends Component {
   }
 
 
+  updateMatchingItemIndices(string){
+
+    //find index of those that begin with string param and store in state
+    let matchingItemsIndices = [];
+
+    this.state.items.forEach( (item, index) => {
+
+      if (item.name.startsWith(string)) {
+
+        matchingItemsIndices.push(index);
+
+      }
+    });
+
+    this.setState({
+      matchingIndices: matchingItemsIndices,
+      visibleItemCount: (matchingItemsIndices.length < 5) ? matchingItemsIndices.length : 5
+    });
+
+  }
+
+  resetState(){
+
+    this.setState({
+      matchingIndices: [],
+      visibleItemCount: 0
+    });
+
+  }
+
+  handleChange(event) {
+
+    //store search string
+    const string = event.target.value;
+    this.updateSearchString(string);
+
+    // find matching items, store matching items in state
+    if (string.length > 2) {
+
+      this.resetState();
+      this.updateMatchingItemIndices(string);
+
+    } else {
+
+      this.resetState();
+
+    }
+  }
+
+
+
 
   render() {
 
@@ -76,12 +145,14 @@ class App extends Component {
 
         <form className="search">
 
-          <input className="search__input"  type="text" />
+          <input className="search__input" type="text" value={this.state.searchValue} onChange={this.handleChange} />
 
 
         </form>
 
-        <DataTable data={this.getItems()} />
+
+
+        {this.state.matchingIndices && this.state.matchingIndices.length > 0 && <DataTable data={this.getItems()} />}
 
         <button className="search__showMore" onClick={this.showMore}>show more</button>
 
